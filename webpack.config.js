0e3a4b8f10682ee4464c19ad2066bc02fe85dfbe
brsyvz <brs2sty@gmail.com>
2021-09-16 17:19:29 +0300

    applied babeljs

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,54 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "production",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    
   },
 
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
       {
         test: /\.css$/i,
 
         use: ["style-loader", "css-loader"],
       },
       {
-
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
 
-        type: 'asset/resource',
-
-      },
-	  {
-        test: /\.mp3$/,
-        loader: 'file-loader',
+        type: "asset/resource",
       },
       {
-
+        test: /\.mp3$/,
+        loader: "file-loader",
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
 
-        type: 'asset/resource',
-
+        type: "asset/resource",
       },
     ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 9000,
+    open: true,
+  },
+  performance: {
+    hints: false,
   },
 };
